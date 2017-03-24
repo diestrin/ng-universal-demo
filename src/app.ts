173474db53471b447838a97139d96310c51e4bad
Diego Barahona <diestrin@gmail.com>
2017-03-24 15:31:23 -0600

    PoC

--- src/app.ts
+++ src/app.ts
@@ -1,19 +1,28 @@
 import { NgModule, Component } from '@angular/core'
 import { BrowserModule } from '@angular/platform-browser'
 import { RouterModule } from '@angular/router'
+import { Jsonp, JsonpModule } from '@angular/http';
+import 'rxjs/add/operator/map'
 
 @Component({
 	selector: 'home-view',
-	template: `<h3>Home View</h3>`
+	template: `<h3>{{title}}</h3>`
 })
-export class HomeView {}
+export class HomeView {
+  public title: string;
+
+  constructor(http: Jsonp) {
+    http.get('https://jsonplaceholder.typicode.com/posts/1?callback=JSONP_CALLBACK')
+      .map(res => res.json())
+      .subscribe(data => this.title = data.title);
+  }
+}
 
 @Component({
 	selector: 'demo-app',
 	template: `
 	  <h1>Universal Demo</h1>
 	  <a routerLink="/">Home</a>
-	  <a routerLink="/lazy">Lazy</a>
 	  <router-outlet></router-outlet>
 	`
 })
@@ -21,12 +30,12 @@ export class AppComponent {}
 
 @NgModule({
 	imports: [
+    JsonpModule,
 		BrowserModule.withServerTransition({
 		  appId: 'universal-demo-app'
 		}),
 		RouterModule.forRoot([
-			{ path: '', component: HomeView, pathMatch: 'full'},
-			{ path: 'lazy', loadChildren: './lazy.module#LazyModule'}
+			{ path: '', component: HomeView, pathMatch: 'full'}
 		])
 	],
 	declarations: [ AppComponent, HomeView ],
